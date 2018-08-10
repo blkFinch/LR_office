ed7ac4e3e21a2f2ec60d9deb79057e40f9bb94ae
KERRINGTON\galen <blkfinch@gmail.com>
2018-08-09 22:17:33 -0600

    working randomized boss summon -- also fixed .gitignore

---
+++ Office Sim/Assets/Scripts/BossManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour {
+
+	Animator door_anim;
+	SpriteRenderer sprite;
+
+
+	// Use this for initialization
+	void Start () {
+		door_anim = GetComponentInChildren<Animator>();
+		sprite = GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	public void SummonBoss(){
+		door_anim.Play("Open");
+		Invoke("RenderBoss", 0.4f );
+	}
+
+	public void DeSummonBoss(){
+		door_anim.Play("Open");
+		Invoke("HideBoss", 0.4f );
+	}
+
+	public void RenderBoss(){
+		sprite.enabled = true;
+	}
+
+	public void HideBoss(){
+		sprite.enabled = false;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Office Sim/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	BossManager bm;
+	public float _bps = 0.2f;
+
+	bool bossIsActive = false;
+
+	// Use this for initialization
+	void Start () {
+		bm = FindObjectOfType<BossManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!bossIsActive){
+			float p = _bps * Time.deltaTime;
+
+	        if(Random.value < p) { ActivateBoss(); }
+		}
+	}
+
+	void ActivateBoss(){
+		ToggleBoss();
+        bm.SummonBoss();
+        Invoke("DeactivateBoss", 2f);
+	}
+
+	void ToggleBoss(){
+		bossIsActive = !bossIsActive;
+	}
+
+	void DeactivateBoss(){
+		bm.DeSummonBoss();
+		Invoke("ToggleBoss", 1f);
+	}
+}
